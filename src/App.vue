5a00470fd5e099022278f28e4e80503ccea1e847
OHusan <omerinovic27@gmail.com>
2023-09-04 23:01:14 +0200

    Installed vue cli, and made initial project pages

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <the-header></the-header>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+export default {
+  components: {
+    TheHeader
+  }
+
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,73 @@
+<template>
+  <header class="wrapper">
+    <div class="container">
+      <p>Test Me</p>
+    </div>
+    <div class="container">
+      <form>
+        <div class="input-container">
+          <label for="username">Username</label>
+          <input type="text" id="username" />
+        </div>
+        <div class="input-container">
+          <label for="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <button class="button">Submit</button>
+      </form>
+    </div>
+  </header>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  background-color: #15769b;
+
+  .container {
+    padding: 0.5rem 2rem;
+
+    p {
+      font-weight: bold;
+      font-size: 24px;
+      color: white;
+    }
+
+    form {
+      display: flex;
+      flex-direction: row;
+      gap: 10px;
+
+      .input-container {
+        label {
+          margin: 0 10px;
+        }
+
+        input {
+          border-radius: 5px;
+          border: 1px solid #ccc;
+        }
+      }
+
+      .button {
+        border-radius: 5px;
+        border: 1px solid #ccc;
+        cursor: pointer;
+
+        &:active,
+        &:hover {
+          background-color: rgb(195, 194, 194);
+        }
+      }
+    }
+  }
+}
+</style>
